07e302c3d86534e821a901cc40b63eccd94ab8d7
Shaun <shaun@SierraAlpha.co.nz>
2020-02-29 16:28:20 +1300

    working to ignore what needs to be ignored

--- elpa/dracula-theme-20200226.1844/dracula-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dracula-theme" "20200226.1844" "Dracula Theme" '((emacs "24")) :commit "5eb9e7b8d88d9e6fd7c8f73229b0b2b9a8d3ed26" :authors '(("film42")) :maintainer '("film42") :url "https://github.com/dracula/emacs")
--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (counsel swiper dracula-theme ace-window org-bullets which-key try use-package))))
+    (swiper-helm counsel swiper dracula-theme ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
