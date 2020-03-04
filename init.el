a91a442a5d552a3351ff07337b216b3414857187
Shaun <shaun@sierraalpha.co.nz>
2020-03-04 22:33:53 +1300

    Reveal mode is seriously cool

--- init.el
+++ init.el
@@ -13,3 +13,17 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/sa-init.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (htmlize ox-reveal which-key use-package try toc-org org-bullets dracula-theme counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
